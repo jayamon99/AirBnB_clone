f63479cac297b49841c78cc543ac0460855e41f0
jayamon99 <henryquaye99@outlook.com>
2022-08-08 14:59:03 +0000

    pycodestyle pass

---
+++ beautiful.py
@@ -0,0 +1,11 @@
+
+class Hero():
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+archer = Hero("Arthur")
+print(archer.get_name())
